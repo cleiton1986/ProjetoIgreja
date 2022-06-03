ece16862a56290eb76b97494e68fd8e81e9e1fda
cleiton1986 <36868612+cleiton1986@users.noreply.github.com>
2022-06-02 22:16:10 -0300

    Formatação automap

--- ProjetoIgreja/Back/src/IgrejaJdLilah.Application/Helpers/EntidadeParaViewModel.cs
+++ ProjetoIgreja/Back/src/IgrejaJdLilah.Application/Helpers/EntidadeParaViewModel.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using AutoMapper;
 using IgrejaJdLilah.Application.Model;
 using IgrejaJdLilah.Domain.Entidades;
@@ -9,12 +10,52 @@ namespace IgrejaJdLilah.Application.Helpers
         public EntidadeParaViewModel()
         {
            // CreateMap<EventoViewModel, Evento>().ReverseMap();
-             CreateMap<EventoViewModel, Evento>();
-            CreateMap<EnderecoViewModel, Endereco>();
-            CreateMap<LoteViewModel, Lote>();
-            CreateMap<PalestranteEventoViewModel, PalestranteEvento>();
-            CreateMap<PalestranteViewModel, Palestrante>();
-            CreateMap<RedeSocialViewModel, RedeSocial>();
+            CreateMap<EventoViewModel, Evento>()
+              .AfterMap((x, y) => x.DataEvento = y.DataEvento.HasValue ? "" : y.DataEvento.Value.ToString("dd/MM/yyyy", new CultureInfo("pt-BR")))
+              .AfterMap((x, y) => x.Email = y.Email)
+              .AfterMap((x, y) => x.Id = y.Id)
+              .AfterMap((x, y) => x.ImagemURL = y.ImagemURL)
+              .AfterMap((x, y) => x.Local = y.Local)
+              .AfterMap((x, y) => x.Observacao = y.Observacao)
+              .AfterMap((x, y) => x.QtdPessoas = y.QtdPessoas)
+              .AfterMap((x, y) => x.Telefone = y.Telefone)
+              .AfterMap((x, y) => x.Tema = y.Tema);
+             
+            CreateMap<EnderecoViewModel, Endereco>()
+            .AfterMap((x, y) => x.Cep = y.Cep)
+            .AfterMap((x, y) => x.Complemento = y.Complemento)
+            .AfterMap((x, y) => x.Id = y.Id)
+            .AfterMap((x, y) => x.Local = y.Local)
+            .AfterMap((x, y) => x.Numero = y.Numero);
+
+            CreateMap<LoteViewModel, Lote>()
+             .AfterMap((x, y) => x.EventoId = y.EventoId)
+             .AfterMap((x, y) => x.Id = y.Id)
+             .AfterMap((x, y) => x.Nome = y.Nome)
+             .AfterMap((x, y) => x.Quantidade = y.Quantidade)
+             .AfterMap((x, y) => x.DataFim = y.DataFim.HasValue ? "" : y.DataFim.Value.ToString("dd/MM/yyyy", new CultureInfo("pt-BR")))
+             .AfterMap((x, y) => x.DataInicio = y.DataInicio.HasValue ? "" : y.DataInicio.Value.ToString("dd/MM/yyyy", new CultureInfo("pt-BR")));
+             
+            CreateMap<PalestranteEventoViewModel, PalestranteEvento>()
+             .AfterMap((x, y) => x.EventoId = y.EventoId)
+             .AfterMap((x, y) => x.PalestranteId = y.PalestranteId);
+
+            CreateMap<PalestranteViewModel, Palestrante>()
+              .AfterMap((x, y) => x.Email = y.Email)
+              .AfterMap((x, y) => x.Denominacao = y.Denominacao)
+              .AfterMap((x, y) => x.EnderecoId = y.EnderecoId)
+              .AfterMap((x, y) => x.FazParteMesmaDenominacao = y.FazParteMesmaDenominacao)
+              .AfterMap((x, y) => x.Id = y.Id)
+              .AfterMap((x, y) => x.ImagemUrl = y.ImagemUrl)
+              .AfterMap((x, y) => x.MiniCurriculo = y.MiniCurriculo)
+              .AfterMap((x, y) => x.DataCadastro = y.DataCadastro != null ? "" : y.DataCadastro.ToString("dd/MM/yyyy", new CultureInfo("pt-BR")));
+
+            CreateMap<RedeSocialViewModel, RedeSocial>()
+              .AfterMap((x, y) => x.EventoId = y.EventoId)
+              .AfterMap((x, y) => x.Id = y.Id)
+              .AfterMap((x, y) => x.Nome = y.Nome)
+              .AfterMap((x, y) => x.PalestranteId = y.PalestranteId)
+              .AfterMap((x, y) => x.Url = y.Url);
             
         }
     }
--- ProjetoIgreja/Back/src/IgrejaJdLilah.Application/Helpers/ViewModelParaEntidade.cs
+++ ProjetoIgreja/Back/src/IgrejaJdLilah.Application/Helpers/ViewModelParaEntidade.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using AutoMapper;
 using IgrejaJdLilah.Application.Model;
 using IgrejaJdLilah.Domain.Entidades;
@@ -8,12 +10,58 @@ namespace IgrejaJdLilah.API.Dtos.Helpers
     {
         public ViewModelParaEntidade()
         {
-            CreateMap<Evento, EventoViewModel>();
-            CreateMap<Endereco, EnderecoViewModel>();
-            CreateMap<Lote, LoteViewModel>();
-            CreateMap<PalestranteEvento, PalestranteEventoViewModel>();
-            CreateMap<Palestrante, PalestranteViewModel>();
-            CreateMap<RedeSocial, RedeSocialViewModel>();
+
+            CreateMap<Evento, EventoViewModel>()
+              .AfterMap((x, y) => x.DataEvento = !string.IsNullOrEmpty(y.DataEvento) ? (DateTime?)null : Convert.ToDateTime(y.DataEvento))
+              .AfterMap((x, y) => x.Email = y.Email)
+              .AfterMap((x, y) => x.Id = y.Id)
+              .AfterMap((x, y) => x.ImagemURL = y.ImagemURL)
+              .AfterMap((x, y) => x.Local = y.Local)
+              .AfterMap((x, y) => x.Observacao = y.Observacao)
+              .AfterMap((x, y) => x.QtdPessoas = y.QtdPessoas)
+              .AfterMap((x, y) => x.Telefone = y.Telefone)
+              .AfterMap((x, y) => x.Tema = y.Tema);
+             
+
+            CreateMap<Endereco, EnderecoViewModel>()
+             .AfterMap((x, y) => x.Cep = y.Cep)
+             .AfterMap((x, y) => x.Complemento = y.Complemento)
+             .AfterMap((x, y) => x.Id = y.Id)
+             .AfterMap((x, y) => x.Local = y.Local)
+             .AfterMap((x, y) => x.Numero = y.Numero);
+
+            CreateMap<Lote, LoteViewModel>()
+              .AfterMap((x, y) => x.EventoId = y.EventoId)
+              .AfterMap((x, y) => x.Id = y.Id)
+              .AfterMap((x, y) => x.Nome = y.Nome)
+              .AfterMap((x, y) => x.Quantidade = y.Quantidade)
+              .AfterMap((x, y) => x.DataFim = !string.IsNullOrEmpty(y.DataFim) ? (DateTime?)null : Convert.ToDateTime(y.DataFim))
+              .AfterMap((x, y) => x.DataInicio = !string.IsNullOrEmpty(y.DataInicio) ? (DateTime?)null : Convert.ToDateTime(y.DataInicio));
+             
+
+            CreateMap<PalestranteEvento, PalestranteEventoViewModel>()
+              .AfterMap((x, y) => x.EventoId = y.EventoId)
+              .AfterMap((x, y) => x.PalestranteId = y.PalestranteId);
+
+
+            CreateMap<Palestrante, PalestranteViewModel>()
+              .AfterMap((x, y) => x.Email = y.Email)
+              .AfterMap((x, y) => x.Denominacao = y.Denominacao)
+              .AfterMap((x, y) => x.EnderecoId = y.EnderecoId)
+              .AfterMap((x, y) => x.FazParteMesmaDenominacao = y.FazParteMesmaDenominacao)
+              .AfterMap((x, y) => x.Id = y.Id)
+              .AfterMap((x, y) => x.ImagemUrl = y.ImagemUrl)
+              .AfterMap((x, y) => x.MiniCurriculo = y.MiniCurriculo)
+              .AfterMap((x, y) => x.DataCadastro =  Convert.ToDateTime(y.DataCadastro));
+             
+             
+            CreateMap<RedeSocial, RedeSocialViewModel>()
+              .AfterMap((x, y) => x.EventoId = y.EventoId)
+              .AfterMap((x, y) => x.Id = y.Id)
+              .AfterMap((x, y) => x.Nome = y.Nome)
+              .AfterMap((x, y) => x.PalestranteId = y.PalestranteId)
+              .AfterMap((x, y) => x.Url = y.Url);
+
         }
         
     }
